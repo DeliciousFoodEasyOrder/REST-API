4aa254f2dbac5a6f2bb4dfa85effeefd3fc951cd
HinanawiTenshi <dr.paper@live.com>
2018-05-04 11:31:16 +0800

    Update merchant.go

--- models/merchant.go
+++ models/merchant.go
@@ -7,3 +7,31 @@ type Merchant struct {
 	Phone      string `json:"phone"`
 	Password   string `json:"password"`
 }
+
+// Insert inserts self to database
+func (m *Merchant) Insert() {
+	_, err := orm.InsertOne(m)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// FindByEmail finds a merchant by email
+func FindByEmail(email string) *Merchant {
+	var merchant Merchant
+	_, err := orm.Table(merchant).Where("Email=?", email).Get(&merchant)
+	if err != nil {
+		panic(err)
+	}
+	return &merchant
+}
+
+// FindByPhone finds a merchant by phone
+func FindByPhone(phone string) *Merchant {
+	var merchant Merchant
+	_, err := orm.Table(merchant).Where("Phone=?", phone).Get(&merchant)
+	if err != nil {
+		panic(err)
+	}
+	return &merchant
+}
