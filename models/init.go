e0a819b45fc5c3c35a31307be18f6a89e5eca48c
HinanawiTenshi <dr.paper@live.com>
2018-05-04 15:54:40 +0800

    Update init.go Change mapper

--- models/init.go
+++ models/init.go
@@ -2,11 +2,12 @@ package models
 
 import (
 	_ "github.com/go-sql-driver/mysql" // Mysql Driver
+	"github.com/go-xorm/core"
 	"github.com/go-xorm/xorm"
 )
 
 const (
-	dbDSN      string = ""
+	dbDSN      string = "easyorder:Passw0r_@/easyorder"
 	timeFormat string = "2006-01-02 15:04:05"
 )
 
@@ -17,6 +18,7 @@ var (
 func init() {
 	var err error
 	orm, err = xorm.NewEngine("mysql", dbDSN)
+	orm.SetMapper(core.SameMapper{})
 	if err != nil {
 		panic(err)
 	}
