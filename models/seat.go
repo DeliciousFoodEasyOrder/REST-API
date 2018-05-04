b10304072cc4a3abc6a9975e52b074670e8c1338
zhang <13719345192@163.com>
2018-05-04 22:02:21 +0800

    finish the curd of seat and food collection

--- models/seat.go
+++ models/seat.go
@@ -2,8 +2,48 @@ package models
 
 // Seat Model
 type Seat struct {
-	SeatID     int    `json:"seat_id"`
+	SeatID     int    `xorm:"AUTOINCR" json:"seat_id"`
 	Number     string `json:"number"`
 	QRCodeURL  string `json:"qr_code_url"`
 	MerchantID int    `json:"merchant_id"`
+}
+
+type SeatDataAccessObject struct{}
+
+var SeatDAO *SeatDataAccessObject
+
+// InsertOne inserts a seat to database
+func (*SeatDataAccessObject) InsertOne(s *Seat) {
+	_, err := orm.InsertOne(s)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// FindByNumber finds a seat by id
+func (*SeatDataAccessObject) FindByID(seatID int) *Seat {
+	var seat Seat
+	has, err := orm.Table(seat).ID(seatID).Get(&seat)
+	if err != nil {
+		panic(err)
+	}
+	if !has {
+		return nil
+	}
+	return &seat
+}
+
+func (*SeatDataAccessObject) DeleteBySeatID(seatID int) {
+	var seat Seat
+	_, err := orm.Table(seat).ID(seatID).Delete(&seat)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (*SeatDataAccessObject) UpdateOne(seat *Seat) {
+	_, err := orm.Table(seat).ID(seat.SeatID).Update(seat)
+	if err != nil {
+		panic(err)
+	}
 }
