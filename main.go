e3734853ecf48bc4a54e3a7e50e250541f138118
HinanawiTenshi <dr.paper@live.com>
2018-05-20 19:30:50 +0800

    Update main.go Upgrade to HTTPS

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import "github.com/DeliciousFoodEasyOrder/REST-API/restapi"
+import (
+	"net/http"
+
+	"github.com/DeliciousFoodEasyOrder/REST-API/restapi"
+)
 
 func main() {
-	port := ":80"
+	port := ":443"
 
 	server := restapi.NewServer()
 
-	server.Run(port)
+	// server.Run(port)
+	http.ListenAndServeTLS(
+		port,
+		"/etc/letsencrypt/live/easyorder.cf/fullchain.pem",
+		"/etc/letsencrypt/live/easyorder.cf/privkey.pem",
+		server,
+	)
 }
