8416a897708a3f423a074411cc7d747559ce78cc
Tendernesszh <13719345192@163.com>
2018-05-27 13:45:54 +0800

    modify collection_customer.go and customer.go

--- restapi/collection_customer.go
+++ restapi/collection_customer.go
@@ -1,8 +1,13 @@
 package restapi
 
 import (
+	"encoding/json"
 	"net/http"
-
+	"github.com/DeliciousFoodEasyOrder/REST-API/models"
+	"regexp"
+	"strconv"
+	"fmt"
+	//"github.com/DeliciousFoodEasyOrder/REST-API/token"
 	"github.com/gorilla/mux"
 )
 
@@ -26,23 +31,126 @@ func routeCustomerCollection(router *mux.Router) {
 func handlerGetCustomer() http.HandlerFunc {
 
 	return func(w http.ResponseWriter, req *http.Request) {
-
+		
+		customerIDStr := mux.Vars(req)["customer_id"]
+	
+		if cond, _ := regexp.MatchString("[1-9][0-9]*", customerIDStr); !cond {
+			formatter.JSON(w, http.StatusBadRequest, NewResp(
+				http.StatusBadRequest,
+				"获取用户信息失败",
+				NewErr("Bad parameters", "id must be a number"),
+			))
+			return
+		}
+	
+		customerID, _ := strconv.Atoi(customerIDStr)
+		customer := models.CustomerDAO.FindByCustomerID(customerID)
+		if customer == nil {
+			formatter.JSON(w, http.StatusBadRequest, NewResp(
+				http.StatusBadRequest,
+				"获取用户信息失败",
+				NewErr("Bad parameters", "customer not found"),
+			))
+			return
+		}
+	
+		formatter.JSON(w, http.StatusOK, NewResp(
+			http.StatusOK,
+			"获取用户信息成功",
+			customer,
+		))
 	}
-
 }
 
 func handlerCreateCustomer() http.HandlerFunc {
 
 	return func(w http.ResponseWriter, req *http.Request) {
-
+		//claims := token.ParseClaims(getTokenString(req))
+		decoder := json.NewDecoder(req.Body)
+		var customer models.Customer
+		err := decoder.Decode(&customer)
+		if err != nil {
+			formatter.JSON(w, http.StatusBadRequest, NewResp(
+				http.StatusBadRequest,
+				"创建用户失败",
+				NewErr("Bad parameters", "please check your request format"),
+			))
+			panic(err)
+		}
+	
+		err = models.CustomerDAO.InsertOne(&customer)
+		if err != nil {
+			formatter.JSON(w, http.StatusInternalServerError, NewResp(
+				http.StatusInternalServerError,
+				"创建用户失败",
+				NewErr("Database error", "see server log for more information"),
+			))
+			panic(err)
+		}
+		formatter.JSON(w, http.StatusCreated, NewResp(
+			http.StatusCreated,
+			"创建用户成功",
+			customer,
+		))
+		
 	}
-
 }
 
 func handlerPatchCustomer() http.HandlerFunc {
 
 	return func(w http.ResponseWriter, req *http.Request) {
+		customerIDStr := mux.Vars(req)["customer_id"]
 
+		if cond, _ := regexp.MatchString("[1-9][0-9]*", customerIDStr); !cond {
+			formatter.JSON(w, http.StatusBadRequest, NewResp(
+				http.StatusBadRequest,
+				"获取用户信息失败",
+				NewErr("Bad parameters", "id must be a number"),
+			))
+			return
+		}
+
+		customerID, _ := strconv.Atoi(customerIDStr)
+		var old *models.Customer
+
+		if old = models.CustomerDAO.FindByCustomerID(customerID); old == nil {
+			formatter.JSON(w, http.StatusBadRequest, NewResp(
+				http.StatusBadRequest,
+				"修改用户信息失败",
+				NewErr("Bad parameters", "customer not found"),
+			))
+			return
+		}
+
+		var data models.Customer
+		decoder := json.NewDecoder(req.Body)
+		decoder.Decode(&data)
+		data.CustomerID = customerID
+		fmt.Println(data)
+
+		/*if data.MerchantID != 0 {
+			formatter.JSON(w, http.StatusBadRequest, NewResp(
+				http.StatusBadRequest,
+				"修改订单失败",
+				NewErr("Bad paramters", "invalid modify"),
+			))
+			return
+		}*/
+
+		customer, err := models.CustomerDAO.UpdateOne(&data)
+		if err != nil {
+			formatter.JSON(w, http.StatusInternalServerError, NewResp(
+				http.StatusInternalServerError,
+				"修改用户信息失败",
+				NewErr("Database error", "see server log for more information"),
+			))
+			panic(err)
+		}
+		formatter.JSON(w, http.StatusCreated, NewResp(
+			http.StatusCreated,
+			"修改用户信息成功",
+			customer,
+		))
 	}
 
 }
