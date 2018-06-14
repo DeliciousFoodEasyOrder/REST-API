daa5819f54e81a5765521e952d69c0507baa2f0c
Hinanawi Tenshi <dr.paper@live.com>
2018-06-14 19:53:10 +0800

    Update users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -1,2 +1,2 @@
-CREATE USER 'easyorder'@'%' IDENTIFIED BY 'Passw0r_';
+CREATE USER 'easyorder'@'172.18.0.0/255.255.0.0' IDENTIFIED BY 'Passw0r_';
 GRANT ALL ON easyorder.* to 'easyorder'@'172.18.0.0/255.255.0.0' WITH GRANT OPTION;
